0ed4e279f32a99115e4fadea58c7e89969c525f0
PabloHenrique <pablohenriquenadai@gmail.com>
2022-01-14 20:10:09 -0300

    MovieCard

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar(){
                 <a href="https://github.com/PabloHenrique">
                 <div className='dsmovie-contact-container'>
                     <GithubIcon />
-                    <p className='dsmovie-contact-link'>/devsuperior</p>
+                    <p className='dsmovie-contact-link'>/pablohenrique</p>
                 </div>
                 </a>
             </div>
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css'
+
 function Form() {
     const movie = {
         id: 1,
